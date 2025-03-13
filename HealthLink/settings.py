d10330a04e86c5142dbcfd228a1d83ac2b4be1fb
Manoj Rathore <manojrathore78900@gmail.com>
2025-03-13 20:20:56 +0530

    Fix middleware error

--- HealthLink/settings.py
+++ HealthLink/settings.py
@@ -48,14 +48,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Ensure it's here if using WhiteNoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.staticfiles',
 ]
 
 ROOT_URLCONF = 'HealthLink.urls'
